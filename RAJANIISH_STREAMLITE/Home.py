8cc1d8eb8bd43c9057c3b110dab695cd634aa4f3
kunal <kunalshrivastava.pune@gmail.com>
2024-02-18 02:02:34 -0800

    first commit

--- RAJANIISH_STREAMLITE/Home.py
+++ RAJANIISH_STREAMLITE/Home.py
@@ -9,7 +9,7 @@ st.set_page_config(
     page_title="Homepage",
     page_icon="🏡",
     layout="centered",
-    initial_sidebar_state="expanded"
+    initial_sidebar_state="collapsed"
 )
 
 # Open YAML file
@@ -29,30 +29,30 @@ authenticator.login(location='sidebar')
 
 if st.session_state["authentication_status"]:
     authenticator.logout()
-    st.write(f'Welcome *{st.session_state["name"]}*')
-    st.title('Some content')
+    st.markdown(f'#### Welcome *{st.session_state["name"]}*')
+
+    col1, col2 = st.columns(2)
+
+    col3, empty_col, col4 = st.columns([1.5, 0.05, 1.5])
+
+    df = pandas.read_csv("Strategies_data.csv", sep=";")
+
+    with col3:
+        for index, row in df[:1].iterrows():
+            st.subheader(row["title"])
+            st.write(row["description"])
+            st.image("images/" + row["image"], width=200)
+            st.write(f"[Source Code]({row['url']})")
+
+
+    with col4:
+        for index, row in df[1:].iterrows():
+            st.subheader(row["title"])
+            st.write(row["description"])
+            st.image("images/" + row["image"], width=200)
+            st.write(f"[Source Code]({row['url']})")
+
 elif st.session_state["authentication_status"] is False:
     st.error('Username/password is incorrect')
 elif st.session_state["authentication_status"] is None:
     st.warning('Please enter your username and password')
-
-col1, col2 = st.columns(2)
-
-col3, empty_col, col4 = st.columns([1.5, 0.05, 1.5])
-
-df = pandas.read_csv("Strategies_data.csv", sep=";")
-
-with col3:
-    for index, row in df[:1].iterrows():
-        st.header(row["title"])
-        st.write(row["description"])
-        st.image("images/" + row["image"])
-        st.write(f"[Source Code]({row['url']})")
-
-
-with col4:
-    for index, row in df[1:].iterrows():
-        st.header(row["title"])
-        st.write(row["description"])
-        st.image("images/" + row["image"])
-        st.write(f"[Source Code]({row['url']})")
--- RAJANIISH_STREAMLITE/pages/2_BackTest.py
+++ RAJANIISH_STREAMLITE/pages/2_BackTest.py
@@ -3,21 +3,27 @@ import pandas
 st.set_page_config(layout="wide")
 col1, col2 = st.columns(2)
 
-col3, empty_col, col4 = st.columns([1.5, 0.05, 1.5])
+if st.session_state["authentication_status"]:
 
-df = pandas.read_csv("BackTest.csv", sep=";")
+    col3, empty_col, col4 = st.columns([1.5, 0.05, 1.5])
 
-with col3:
-    for index, row in df[:1].iterrows():
-        st.header(row["title"])
-        st.write(row["description"])
-        st.image("images/" + row["image"])
-        st.write(f"[Source Code]({row['url']})")
+    df = pandas.read_csv("BackTest.csv", sep=";")
+
+    with col3:
+        for index, row in df[:1].iterrows():
+            st.header(row["title"])
+            st.write(row["description"])
+            st.image("images/" + row["image"])
+            st.write(f"[Source Code]({row['url']})")
 
 
-with col4:
-    for index, row in df[1:].iterrows():
-        st.header(row["title"])
-        st.write(row["description"])
-        st.image("images/" + row["image"])
-        st.write(f"[Source Code]({row['url']})")
+    with col4:
+        for index, row in df[1:].iterrows():
+            st.header(row["title"])
+            st.write(row["description"])
+            st.image("images/" + row["image"])
+            st.write(f"[Source Code]({row['url']})")
+elif st.session_state["authentication_status"] is False:
+    st.error('Username/password is incorrect')
+elif st.session_state["authentication_status"] is None:
+    st.warning('Please enter your username and password')
