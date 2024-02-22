3976ee73dc8da61f02d485432538607dc12353d6
Kunal Shrivastava <kush0221@netcracker.com>
2024-02-23 04:33:19 +0530

    first commit

--- RAJANIISH_STREAMLITE/pages/1_Strategies.py
+++ RAJANIISH_STREAMLITE/pages/1_Strategies.py
@@ -1,8 +1,6 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-import nsepython as np
 import DeltaDisp as dd
 st.set_page_config(layout="wide")
 
@@ -48,17 +46,35 @@ if st.session_state["authentication_status"]:
 
     st.divider()
 
-    st.markdown("#### Price Tracker")
+    st.markdown("#### History Details")
     with st.expander("Click to Expand"):
         with st.form("population-form1"):
-            col1, col2, col3 = st.columns(3)
+            col1, col2, col3, col4  = st.columns(4)
             with col1:
-                st.write("Choose a starting date")
+                st.write("Equity")
+                st.write("Index")
+                st.write("Options")                
             with col2:
-                st.write("Choose an end date")
+                options = ['Option 1', 'Option 2', 'Option 3']
+                options1 = ['Option 1', 'Option 2', 'Option 3']
+                options2 = ['Option 1', 'Option 2', 'Option 3']
+                selected_option = st.selectbox('Select an option:', options , key = '1' )
+                selected_option1 = st.selectbox('Select an option:', options1 , key = '2')
+                selected_option2 = st.selectbox('Select an option:', options2 , key = '3' )
             with col3:
-                st.write("Choose a location")
-            submit_btn = st.form_submit_button("Analyze", type="primary")
+                options3 = ['Option 1', 'Option 2', 'Option 3']
+                options4 = ['Option 1', 'Option 2', 'Option 3']
+                options5 = ['Option 1', 'Option 2', 'Option 3']
+                selected_option3 = st.selectbox('Select an option:', options3, key = '4')
+                selected_option4 = st.selectbox('Select an option:', options4, key = '5')
+                selected_option5 = st.selectbox('Select an option:', options5, key = '6')
+            with col4:
+                options6 = ['Option 1', 'Option 2', 'Option 3']
+                options7 = ['Option 1', 'Option 2', 'Option 3']             
+                selected_option6 = st.selectbox('Select an option:', options6, key = '7')
+                selected_option7 = st.selectbox('Select an option:', options7, key = '8')
+
+            submit_btn = st.form_submit_button("OK", type="primary")
 
 
 
---
+++ RAJANIISH_STREAMLITE/pull_nseindex.py
@@ -0,0 +1,83 @@
+import pandas as pd
+import copy
+import sqlite3 as sq
+from py5paisa import FivePaisaClient
+
+cred={
+    "APP_NAME":"5P55638300",
+    "APP_SOURCE":"9216",
+    "USER_ID":"NQt3w6KdIS0",
+    "PASSWORD":"CCByL3QGV4i",
+    "USER_KEY":"zIImBvHQcKS37KNbefLn5NOs6ioiYACm",
+    "ENCRYPTION_KEY":"QXcUSr0dNFc3GdSC0FLQpwG2pYGggTmY"
+    }
+
+client = FivePaisaClient(cred=cred)
+
+# OAUTH Approach
+# First get a token by logging in to -> https://dev-openapi.5paisa.com/WebVendorLogin/VLogin/Index?VendorKey=zIImBvHQcKS37KNbefLn5NOs6ioiYACm&ResponseURL=localhost
+# VendorKey is UesrKey for individuals user
+# for e.g. you can use ResponseURL as https://www.5paisa.com/technology/developer-apis
+# Pass the token received in the response url after successful login to get an access token (this also sets the token for all the APIs you use)-
+# Please note that you need to copy the request token from URL and paste in this code and start the code within 30s.
+#client.get_oauth_session('Your Response Token')
+
+client.get_oauth_session('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6IjU1NjM4MzAwIiwicm9sZSI6InpJSW1CdkhRY0tTMzdLTmJlZkxuNU5PczZpb2lZQUNtIiwiU3RhdGUiOiIiLCJuYmYiOjE3MDg1NjY0ODMsImV4cCI6MTcwODU2NjU0MywiaWF0IjoxNzA4NTY2NDgzfQ.loUrf0a48DqJ2UlSfXHNWPwZu4oEV16praUEqt9hack')
+
+#historical_data(exchange,exchange type,scrip code,time frame,from data,to date)
+#https://images.5paisa.com/website/scripmaster-csv-format.csv
+# Note : TimeFrame Should be from this list ['1m','5m','10m','15m','30m','60m','1d']
+
+df_nse_100 = pd.read_csv("C:/NSE/STOCKS/ind_index.csv")
+ohlcv = {}
+for index, row in df_nse_100.iterrows():
+    print(row['Symbol'], row['sccode'])
+    df1=client.historical_data('N','C',row['sccode'],'5m','2024-01-01','2024-02-21')
+    #print(df1)
+    df1=client.historical_data('N','C',row['sccode'],'5m','2020-12-17','2021-06-16')
+    #print(df1)
+    df2=client.historical_data('N','C',row['sccode'],'5m','2021-06-17','2021-12-16')
+    #print(df2)
+    df3=client.historical_data('N','C',row['sccode'],'5m','2021-12-17','2022-06-16')
+    #print(df3)
+    df4=client.historical_data('N','C',row['sccode'],'5m','2022-06-17','2022-12-16')
+    #print(df4)
+    df5=client.historical_data('N','C',row['sccode'],'5m','2022-12-17','2023-06-16')
+    #print(df5)
+    df6=client.historical_data('N','C',row['sccode'],'5m','2023-06-17','2023-12-16')
+    #print(df6)
+    df7=client.historical_data('N','C',row['sccode'],'5m','2023-12-17','2024-01-14')
+    #print(df7)
+    df = pd.concat([df1, df2, df3, df4, df5, df6, df7 ], ignore_index=True)
+    ohlcv[row['Symbol']] = df
+
+
+
+
+tickers = df_nse_100["Symbol"].to_list()
+
+
+
+df = copy.deepcopy(ohlcv)
+df_full = pd.DataFrame()
+
+for ticker in tickers:
+  if len(df[ticker]) > 80:
+    print("merging for ",ticker)
+    ohlcv[ticker]["ticker"] = ticker
+    ohlcv[ticker]["Date"] = ohlcv[ticker].index
+    df_full = pd.concat( [ df_full , ohlcv[ticker] ]  )
+
+
+
+
+
+sql_data = 'C:\\NSE\\SA.sqlite2'
+conn = sq.connect(sql_data)
+cur = conn.cursor()
+
+
+cur.execute('''DROP TABLE IF EXISTS NINDEX_OHLC''')
+
+df_full.to_sql('NINDEX_OHLC', conn, if_exists='replace', index=True) # - writes the pd.df to SQLIte DB
+
