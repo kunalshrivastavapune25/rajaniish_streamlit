8bd0ce0c2f0b948daab505a21f3d78ea3ffc22ce
Kunal Shrivastava <kush0221@netcracker.com>
2024-03-10 11:57:21 +0530

    first commit

---
+++ RAJANIISH_STREAMLITE/schedule.bat
@@ -0,0 +1,6 @@
+@echo off
+cd C:\RAJANIISH_STREAMLITE\rajaniish_streamlit\RAJANIISH_STREAMLITE
+call "C:\Tools\MiniConda\Scripts\activate.bat"
+call conda activate streamlit-env
+streamlit run Home.py
+
